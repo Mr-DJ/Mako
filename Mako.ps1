451695d2eeb085d7e4e9421d22d3c79f39813d3f
Samuel Jonathan <jonathandinesh@gmail.com>
2022-03-12 20:09:47 +0530

    initial setup

---
+++ Mako.ps1
@@ -0,0 +1,52 @@
+﻿Write-Host "NOTE: Mako is still under development."
+Write-Host "`nTo begin, press:"
+Write-Host "[1] Init new starting point"
+Write-Host "[2] Read existing starting point"
+
+$mode = Read-Host -Prompt "Start mode ([1]/[2])"
+
+Function generateHash($path) {
+    $hash = Get-FileHash -Path $path -Algorithm SHA512
+    return $hash
+}
+
+Function resetStartingPoint() {
+    $point= Test-Path -Path .\starting-point.txt
+    if($point) {
+        #delete starting point
+        Remove-Item -Path .\starting-point.txt
+    }
+}
+
+if($mode -eq "1") {
+    #if starting point exists already
+    resetStartingPoint
+    
+    #calculate hashes for the target files and store in starting-point.txt
+    Write-Host "[+] Calculating hashes. Create new starting point." -ForegroundColor Green
+
+    #collect target files
+    $target = Get-ChildItem -Path .
+    
+    #collect file hashes
+    foreach($file in $target) {
+        $hash = generateHash $file.FullName
+        if($null -ne $hash.Hash) {
+            "$($hash.Path) | $($hash.Hash)" | Out-File -FilePath .\starting-point.txt -Append
+        }
+    }
+
+} elseif($mode -eq "2") {
+    Write-Host "[!] Read existing starting-point.txt. Monitor target files" -ForegroundColor Yellow
+    #load hash into hash table
+    $hashTable = @{}
+    $path_Hash = Get-Content -Path .\starting-point.txt
+    foreach($pair in $path_Hash) {
+        $hashTable.add($pair.Split("|")[0], $pair.Split("|")[1])
+    }
+    $hashTable
+    #monitor existing files with saved starting point
+} else {
+    #display appropriate response
+    Write-Host "[x] Mode does not exist. Please enter a valid mode." -ForegroundColor Magenta
+} 
